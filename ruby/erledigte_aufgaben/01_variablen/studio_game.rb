9cc082f790aa41ea54bdf8167d2c115d608ad0eb
ralf-g16 <cech96@web.de>
2015-09-04 11:20:26 +0200

    zweite ruby stunde

---
+++ ruby/erledigte_aufgaben/01_variablen/studio_game.rb
@@ -0,0 +1,11 @@
+name1 = "larry"
+health = 60
+puts "#{name1}'s health is #{health}"
+puts name1 + "'s health is " + health.to_s
+puts "#{name1}'s health is #{health * 3 }"
+puts "#{name1}'s health is #{health / 9 } #{health.to_f / 9} #{health / 9.0}"
+puts "erzeugen Sie die folgende Ausgabe: \n\tPlayers: \n\t\t larry  \n\t\t curly  \n\t\t moe "
+larry = "larry"
+curly = "curly"
+moe = "moe"
+puts "erzeugen Sie die folgende Ausgabe: \n\tPlayers: \n\t\t #{larry} \n\t\t #{curly}  \n\t\t #{moe}"
