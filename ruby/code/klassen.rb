f44a6cfcc5f389b8476310c4f8dd08bdc7323f94
ralf-g16 <cech96@web.de>
2015-09-11 11:28:33 +0200

    Ruby Aufgaben Klassen

--- ruby/code/klassen.rb
+++ ruby/code/klassen.rb
@@ -10,9 +10,22 @@ class Movie
 		"#{@title} hat ein Ranking von: #{@rank}."
 	end
 
+	def thumps_up
+		@rank += 1
+		#@rank = @rank + 1
+	end
+	def thumps_down
+		@rank -= 1
+		#@rank = @rank - 1
+	end
+
 end
 
 obj_movie1 = Movie.new('Ninjo', 4)
 obj_movie2 = Movie.new('Ninja')
 puts obj_movie1.to_s
 puts obj_movie2.to_s
+obj_movie1.thumps_up
+puts obj_movie1.to_s
+
+
