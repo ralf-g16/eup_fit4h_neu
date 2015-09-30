c320ca5158f5f9b8074c9e3483568893582bdb56
ralf-g16 <cech96@web.de>
2015-09-30 22:24:36 +0200

    Mehrdimensionale Arrays

---
+++ ruby/erledigte_aufgaben/10_uebungen_array/uebungen_array.rb
@@ -0,0 +1,100 @@
+#1
+#---------------
+# zahlen = (15..20).to_a
+# zahlen_v2 = [15, 16, 17, 18, 19, 20]
+# #a
+# #------------------------------------------------------------------------------------
+# #puts zahlen.join
+# zahlen_v2.size.times { |i| puts zahlen[i] }
+# #b 
+# #------------------------------------------------------------------------------------
+# counter = 0
+# zahlen.size.times do 
+# 	puts "Index: #{counter} - Wert: #{zahlen[counter]}"
+# 	counter = counter + 1
+# end
+
+# #oder
+# zahlen_v2.size.times { |i| puts "Index: #{i} - Wert: #{zahlen_v2[i]}" }
+
+#c 
+#------------------------------------------------------------------------------------
+# a = {}
+# zahlen_v3 = [15, 16, 17, 18, 19, 20]
+# zahlen_v3.size.times { |q| a = Hash[zahlen_v3[q], rand(100)+100]; puts "#{a}"}
+
+# #oder
+
+# zahlen = (15..20).to_a
+# b = {}
+# counter = 0
+# zahlen.size.times do 
+# 	b = Hash[zahlen[counter], rand(100)+100]
+# 	puts "#{b}"
+# 	counter = counter + 1
+# end
+
+# #d
+# #-----------------------------------------------------------------------------------
+# zahlen.size.times { |i| b = { zahlen[i] => rand(100) + 100 }; b.each { |key, value| puts "Index: #{key} - Wert: #{value}" } }
+
+# #oder
+
+# zahlen_v4 = (15..20).to_a
+# c = {}
+# counter = 0
+# zahlen_v4.size.times do 
+# 	b = Hash[zahlen_v4[counter], rand(100)+100]
+# 	b.each { |key,value| puts "Index: #{key} - Wert: #{value}"}
+# 	counter = counter + 1
+# end
+
+# #e
+# #-----------------------------------------------------------------------------------
+
+# zahlen.size.times { |i| c = { zahlen[i] => rand(100) + 100} ; c.each { |key, value| if value > 150; puts "Index: #{key} - Wert: #{value} WOW" else puts "Index: #{key} - Wert: #{value}" end } }
+
+# #oder
+
+# zahlen_v5 = (15..20).to_a
+# c = {}
+# counter = 0
+# zahlen_v5.size.times do 
+# 	c = Hash[zahlen_v5[counter], rand(100)+100]
+# 	c.each { |key,value| if value > 150; puts "Index: #{key} - Wert: #{value} WOW" else puts "Index: #{key} - Wert: #{value}" end} 
+# 	counter = counter + 1	
+# end
+
+#f
+#-----------------------------------------------------------------------------------
+
+# zahlen_v6 = (15..20).to_a
+# f = {}
+# square = Array.new
+# counter = 0
+# zahlen_v6.size.times do 
+# 	f = Hash[zahlen_v6[counter], rand(100)+100]
+# 	f.each { |key,value| puts "Index: #{key} - Wert: #{value}"; square << value * value; puts "#{square[counter]}" }
+# 	counter = counter + 1
+# end
+
+# #g
+#-----------------------------------------------------------------------------------
+
+zahlen_v7 = (15..20).to_a
+f = {}
+square_v2 = Array.new
+counter = 0
+zahlen_v7.size.times do 
+	g = Hash[zahlen_v7[counter], rand(100)+100]
+	g.each { |key,value| puts "Index: #{key} - Wert: #{value}"; square_v2 << value * value; puts "#{square_v2[counter]}" }
+	counter = counter + 1
+end
+
+square_v3 = Array.new
+counter = 0
+square_v2.size.times do 
+	square_v3 = Hash[square_v2[counter], rand(100)+100]
+	square_v3.each { |key,value| puts "Index: #{key} - Wert: #{value}";  puts square_v3 }
+	counter = counter + 1
+end
